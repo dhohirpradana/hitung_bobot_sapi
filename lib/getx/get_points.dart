218b277584a4d7ab5065a47462a0d48197426247
dhohir pradana <49935816+dhohirpradana@users.noreply.github.com>
2022-01-30 18:26:06 +0700

    Update get_points.dart

--- lib/getx/get_points.dart
+++ lib/getx/get_points.dart
@@ -1,19 +1,20 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class PointsController extends GetxController {
+  // ! Koordinat LCD/ Layar
   List<Offset> points = [];
+  // ! Ui/ Tampilan titik di aplikasi
   List<Widget> children = <Widget>[];
-  double panjang = 0;
-  double lebar = 0;
-  // Metode Schoorl
+  // ! Panjang Badan dan Lingkar Dada
+  double pb = 0;
+  double ld = 0;
+
   double berat = 0;
   String halamanText = 'BERANDA';
   void addPoint(Offset data) {
     points.add(data);
-    // children.add(
-    //     // PointWidget.id(id: points.length - 1, offset: data),
-    //     Text(points.length.toString()));
     children.add(Positioned(
         left: data.dx,
         top: data.dy,
@@ -22,35 +23,44 @@ class PointsController extends GetxController {
             child: Text((points.length < 3) ? 'PB' : 'LD',
                 style: const TextStyle(color: Colors.yellow)))));
     if (points.length > 1) {
-      print((points[0] - points[1]).distance);
-      panjang = (points[0] - points[1]).distance * 4.000;
+      // ! Panjang Badan
+      print("PB = " + (points[0] - points[1]).distance.toString());
+      // ! Titik adalah Koma
+      pb = (points[0] - points[1]).distance * 4.000;
     }
     if (points.length > 3) {
-      lebar = ((points[2] - points[3]).distance * 4.000) * 2;
-      // final lebarInch = lebar / 2.54;
-      berat = (panjang + (lebar * lebar)) / 10840;
+      // ! Lingkar Dada (2x Lebar Dada)
+      print("LD = " + (points[2] - points[3]).distance.toString());
+      // ! Titik adalah Koma
+      ld = ((points[2] - points[3]).distance * 4.000) * 2;
+      // ! Rumus menggunakan metode Schoorl untuk memprediksi bobot Sapi
+      // * RUMUS Schoorl : (PB + (LD pangkat 2)) / 10840
+      berat = (pb + (pow(ld, 2))) / 10840;
+      // Pow = Pangkat
     }
     update();
   }
 
+  // ! Hapus titik satu per satu
   void deletePoint() {
     (points.isNotEmpty) ? points.removeLast() : () {};
     (children.isNotEmpty) ? children.removeLast() : () {};
     if (points.length < 2) {
-      panjang = 0;
+      pb = 0;
       berat = 0;
     } else if (points.length > 2 && points.length < 4) {
-      lebar = 0;
+      ld = 0;
       berat = 0;
     }
     update();
   }
 
+  // ! Hapus semua titik
   void clearPoints() {
     (points.isNotEmpty) ? points.clear() : () {};
     (children.isNotEmpty) ? children.clear() : () {};
-    panjang = 0;
-    lebar = 0;
+    pb = 0;
+    ld = 0;
     berat = 0;
     update();
   }
